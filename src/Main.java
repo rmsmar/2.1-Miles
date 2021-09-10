11b5c598c890752f94b73db16a6d8262255c5e86
Marina <rms.marina89@gmail.com>
2021-09-10 14:13:14 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 15000;
+        int pricePerMile = 20;
+        int numberMiles = ticketPrice / pricePerMile;
+        System.out.println(numberMiles + " miles");
+    }
+}
